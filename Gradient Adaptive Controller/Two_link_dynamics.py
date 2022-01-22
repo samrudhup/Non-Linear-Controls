d6d2c5397b189a1a3f7e2fa1e58cc095ae0c585c
Samrudh Untgod Preetham <68507731+samrudhup@users.noreply.github.com>
2022-01-22 17:34:37 -0500

    Add files via upload

---
+++ Gradient Adaptive Controller/Two_link_dynamics.py
@@ -0,0 +1,230 @@
+import numpy as np
+from math import sin
+from math import cos
+
+#Defining a class dynamics
+class dynamics():
+    def __init__(self, alpha=0.2*np.ones(2,dtype=np.float32), beta=0.1*np.ones(2, dtype=np.float32), gamma=0.01*np.ones(5,dtype=np.float32)):
+        
+        #defining the gaines
+        self.alpha=np.diag(alpha)
+        self.beta=np.diag(beta)
+        self.Gamma=np.diag(gamma)
+        
+        #defining the rigid body parameters
+        self.m=np.array([2.0, 2.0],dtype=np.float32)
+        self.l=np.array([0.5, 0.5],dtype=np.float32)
+        self.mBounds=np.array([1.0, 3.0], dtype=np.float32)
+        self.lBounds=np.array([0.25, 0.75],dtype=np.float32)
+        self.g=9.81
+        
+        #defining the desired trajectory parameters
+        self.phid=np.array([np.pi/8, np.pi/4],dtype=np.float32)
+        self.freq=0.2
+        self.a=np.pi/2
+        self.b=np.array([np.pi/2, np.pi/4],dtype=(np.float32))
+        
+        #Initialize state
+        self.phi,_,_=self.getDesiredstate(0.0) #seting the inititial angles to the desired angles
+        self.phiD=np.zeros(2,dtype=np.float32) #initial angular velocity 
+        self.phiDD=np.zeros(2,dtype=(np.float32))   #initial angular acceleration
+        
+        # unknown parameters
+        self.theta = self.getTheta(self.m, self.l)
+        self.thetaH= self.getTheta(self.mBounds[0]*np.ones(2,dtype=np.float32),self.lBounds[0]*np.ones(2,dtype=np.float32))
+        
+        
+    def getDesiredstate(self,t):
+        #To get the desired anglular displacements
+        
+        phid=np.array([self.phid[0]*sin(2*np.pi*self.freq*t-self.a)-self.b[0],
+                     self.phid[1]*sin(2*np.pi*self.freq*t-self.a)+self.b[1]], dtype=np.float32)
+        
+        #To get the desired angular velocity
+        phiDd=np.array([2*np.pi*self.freq*self.phid[0]*cos(2*np.pi*self.freq*t- self.a),
+                       2*np.pi*self.freq*self.phid[1]*cos(2*np.pi*self.freq*t- self.a)], dtype=np.float32)
+        
+        #To get the desired angular acceleration
+        phiDDd=np.array([-((2*np.pi*self.freq)**2)*self.phid[0]*sin(2*np.pi*self.freq*t - self.a),
+                        -((2*np.pi*self.freq)**2)*self.phid[1]*sin(2*np.pi*self.freq*t - self.a)], dtype=np.float32)
+        
+        return phid, phiDd, phiDDd
+   
+    
+    def getTheta(self,m,l):
+    
+        theta = np.array([(m[0]+m[1])*l[0]**2+m[1]*l[1]**2,
+                          m[1]*l[0]*l[1],
+                          m[1]*l[1]**2,
+                          (m[0]+m[1])*l[0],
+                          m[1]*l[1]],dtype=np.float32)
+        return theta
+    
+    #Returns the inertial matrix
+    def getM(self,m,l,phi):
+        
+        m1=m[0]
+        m2=m[1]
+        c1=cos(phi[0])
+        c2=cos(phi[1])
+        s1=sin(phi[0])
+        s2=sin(phi[1])
+        l1=l[0]
+        l2=l[1]
+        
+        M= np.array([[(m1*l1**2)+m2*((l1**2)+2*l1*l2*c2),(m2*(l1*l2*c2 + l2**2))],
+                    [(m2*(l1*l2*c2) + l2**2), (m2*l2**2)]], dtype=np.float32)
+        return M
+                    
+    # Returns the Centripital Coriolis matirx                
+    def getC(self,m,l,phi,phiD):
+        m1=m[0]
+        m2=m[1]
+        c1=cos(phi[0])
+        c2=cos(phi[1])
+        s1=sin(phi[0])
+        s2=sin(phi[1])
+        l1=l[0]
+        l2=l[1]
+        
+        C=np.array([(-2*m2*l1*l2*s2*phiD[0]*phiD[1]- m2*l1*l2*s2*(phiD[1]**2)),
+                    (m2*l1*l2*s2*phi[0]**2)], dtype=np.float32)
+        return C
+    #Returns the Grativational Matrix    
+    def getG(self, m, l, g):
+        m1=m[0]
+        m2=m[1]
+        c1=cos(self.phi[0])
+        c2=cos(self.phi[1])
+        s1=sin(self.phi[0])
+        s2=sin(self.phi[1])
+        c12=cos(self.phi[0]+self.phi[1])
+        s12=sin(self.phi[0]+self.phi[1])
+        l1=l[0]
+        l2=l[1]
+                        
+        G=np.array([((m1+m2)*g*l1*c1 +m2*g*l2*c12),
+                    (m2*g*l2*c12)], dtype=np.float32)    
+        return G
+    
+    #Returns the inertial matrix regressor
+    def getYM(self, vphi, phi):
+        
+        c1=cos(phi[0])
+        c2=cos(phi[1])
+        
+        YM=np.array([[vphi[0], 2*c1*vphi[0], vphi[1], 0.0, 0.0],
+                      [0.0, c2*vphi[0], vphi[0]+vphi[1], 0.0, 0.0]], dtype=np.float32)
+        
+        return YM
+    
+    #Returns the centripital coriolis matrix regressor
+    def getYC(self, phi, phiD):
+        
+        c1=cos(phi[0])
+        c2=cos(phi[1])
+        s1=sin(self.phi[0])
+        s2=sin(self.phi[1])
+        
+        YC= np.array([[ 0.0, -2*s2*phiD[0]*phiD[1]-s2*phiD[1]**2, 0.0, 0.0, 0.0],
+                       [0.0, s2*phiD[0]**2, 0.0, 0.0, 0.0]], dtype=np.float32)
+        return YC
+    
+    #Returns the Gravitational matrix regressor
+    def getYG(self, phi, g):
+        c1=cos(phi[0])
+        c2=cos(phi[1])
+        s1=sin(phi[0])
+        s2=sin(phi[1])
+        c12=cos(phi[0]+phi[1])
+        s12=sin(phi[0]+phi[1])
+        
+        YG=np.array([[ 0.0, 0.0, 0.0, g*c1, g*c12],
+                      [0.0, 0.0, 0.0, 0.0, g*c12]], dtype=(np.float32))
+        return YG
+    
+    #Returns the M_dot matrix regressor
+    def getYM_dot(self, phi, phiD, r):
+        c1=cos(phi[0])
+        c2=cos(phi[1])
+        s1=sin(phi[0])
+        s2=sin(phi[1])
+        
+        YM_dot=np.array([[ 0.0, -2*s2*phiD[1]*r[0]-s2*phiD[1]*r[1], 0.0, 0.0, 0.0],
+                          [0.0, -s2*phiD[1]*r[0], 0.0, 0.0, 0.0]], dtype=(np.float32))
+        return YM_dot
+    
+    #returns the state
+    def getState(self, t):
+        
+        return self.phi, self.phiD, self.phiDD, self.thetaH
+    
+    #returns the error states
+    def getErrorStates(self, t):
+        
+        #gets the desired states
+        phid, phiDd, phiDDd= self.getDesiredstate(t)
+        
+        #gets the errors
+        e = phid - self.phi
+        eD = phiDd - self.phiD
+        r = eD + self.alpha@e
+        
+        #calculate thetatilda
+        
+        thetatilda=self.theta-self.thetaH
+        
+        return e, eD, r, thetatilda
+    
+    #returns the inputs and update law
+    def getTauThetaHD(self, t):
+        #gets the desired states
+        _,_, phiDDd= self.getDesiredstate(t)
+        
+        #get the error
+        e,eD,r,_=self.getErrorStates(t)
+        
+        #get the regressors
+        vphi = phiDDd + self.alpha@eD
+        YM = self.getYM(vphi, self.phi)
+        YC =self.getYC(self.phi, self.phiD)
+        YG =self.getYG(self.phi, self.g)
+        YM_dot=self.getYM_dot(self.phi, self.phiD, r)
+        Y = YM +YC +YG + YM_dot
+        
+        #calculating the contoroller's update law
+        taufb=e+self.beta@r
+        tauff=Y@self.thetaH
+        tau = taufb +tauff
+        
+        
+        thetaHD = self.Gamma@Y.T@r
+        return tau, thetaHD, tauff, taufb
+    
+    #steping the system
+    def step(self, dt, t):
+        #get the dynamics
+        M = self.getM(self.m, self.l, self.phi)
+        C =self.getC(self.m, self.l, self.phi, self.phiD)
+        G = self.getC(self.m, self.l, self.phi, self.phiD)
+        
+        #get the input and the update law
+        tau, thetaHD,_,_ =self.getTauThetaHD(t)
+        
+        #calculate the dynamics using the input
+        self.phiDD =np.linalg.inv(M)@(-C-G+tau)
+        
+        #update the internal states
+        self.phi += dt*self.phiD
+        self.phiD += dt*self.phiDD
+        self.thetaH += dt*thetaHD
+            
+            
+    
+    
+                    
+    
+    
+    
+                          
+        
---
+++ Gradient Adaptive Controller/Two_link_sim.py
@@ -0,0 +1,205 @@
+import numpy as np
+import Two_link_dynamics
+import csv
+import os
+import datetime
+import matplotlib
+import matplotlib.pyplot as plot
+from matplotlib import rc
+
+
+if __name__=='__main__':
+    dt=0.005 #time interval
+    tf=60.0 #final time
+    t=np.linspace(0.0, tf, int(tf/dt), dtype=(np.float32))
+    alpha= 4.0*np.ones(2,dtype=np.float32)
+    beta= 0.25*np.ones(2,dtype=np.float32)
+    gamma= 0.1*np.ones(5,dtype=np.float32)
+    dyn= Two_link_dynamics.dynamics(alpha=alpha, beta=beta, gamma=gamma)
+    phiHist = np.zeros((2,len(t)),dtype=np.float32)
+    phiDHist= np.zeros((2,len(t)),dtype=np.float32)
+    phiDDHist= np.zeros((2,len(t)),dtype=np.float32)
+    phidHist= np.zeros((2,len(t)),dtype=np.float32)
+    phiDdHist= np.zeros((2,len(t)),dtype=np.float32)
+    phiDDdHist= np.zeros((2,len(t)),dtype=np.float32)
+    eHist= np.zeros((2,len(t)),dtype=np.float32)
+    eNormHist= np.zeros_like(t)
+    rHist=np.zeros((2,len(t)),dtype=np.float32)
+    rNormHist= np.zeros_like(t)
+    thetaTildaHist=np.zeros((5,len(t)),dtype=np.float32)
+    thetaNormHist=np.zeros_like(t)
+    tauHist=np.zeros((2,len(t)),dtype=np.float32)
+    tauffHist=np.zeros((2,len(t)),dtype=np.float32)
+    taufbHist=np.zeros((2,len(t)),dtype=np.float32)
+    
+    #start save file
+    savePath="/Users/sam/Documents/Spring 2022"
+    now=datetime.datetime.now()
+    nownew = now.strftime("%Y-%m-%d-%H-%M-%S")
+    path=savePath+"/sim-"+nownew
+    os.mkdir(path)
+    file=open(path+"/data.csv","w",newline='')
+    
+    #writting the header into the file
+    with file: 
+        write = csv.writer(file)
+        write.writerow(["time","e1","e2","r1","r2","tau1","tau2"])
+    file.close()
+    
+    #loop through
+    for jj in range(0, len(t)):
+        #get the states and input data
+        phij, phiDj, phiDDj, thetaHj = dyn.getState(t[jj])
+        phidj, phiDdj, phiDDdj = dyn.getDesiredstate(t[jj])
+        ej,_,rj,thetaTildaj = dyn.getErrorStates(t[jj])
+        tauj,_,tauffj, taufbj = dyn.getTauThetaHD(t[jj])
+        
+        #save the data to the buffers
+        phiHist[:,jj]=phij
+        phiDHist[:,jj]=phiDj
+        phiDDHist[:,jj]=phiDDj
+        phidHist[:,jj]= phidj
+        phiDdHist[:,jj]=phiDdj
+        phiDDdHist[:,jj]=phiDDdj
+        eHist[:,jj]=ej
+        eNormHist[jj]=np.linalg.norm(ej)
+        rHist[:,jj]=rj
+        rNormHist[jj]=np.linalg.norm(rj)
+        thetaTildaHist[:,jj]=thetaTildaj
+        thetaNormHist[jj]=np.linalg.norm(thetaTildaj)
+        tauHist[:,jj]=tauj
+        taufbHist[:,jj]=taufbj
+        tauffHist[:,jj]=tauffj
+        
+        #save the internal data to file
+        file = open(path+"/data.csv","a",newline='')
+        #writing the data into the file
+        with file: 
+            write = csv.writer(file)
+            write.writerow([t[jj],eHist[0,jj],eHist[1,jj],rHist[0,jj],rHist[1,jj],tauHist[0,jj],tauHist[1,jj]])
+        file.close()
+        
+        #step the internal state of the dynamics
+        dyn.step(dt,t[jj])
+    
+    #plot the data
+    #plot teh angles
+    phiplot,phiax = plot.subplots()
+    phiax.plot(t,phidHist[0,:],color='orange',linewidth=2,linestyle='--')
+    phiax.plot(t,phiHist[0,:],color='orange',linewidth=2,linestyle='-')
+    phiax.plot(t,phidHist[1,:],color='blue',linewidth=2,linestyle='--')
+    phiax.plot(t,phiHist[1,:],color='blue',linewidth=2,linestyle='-')
+    phiax.set_xlabel("$t$ $(sec)$")
+    phiax.set_ylabel("$\phi_i$ $(rad)$")
+    phiax.set_title("Angle")
+    phiax.legend(["$\phi_{1d}$","$\phi_1$","$\phi_{2d}$","$\phi_2$"],loc='upper right')
+    phiax.grid()
+    phiplot.savefig(path+"/angles.pdf")
+    
+    #plot the error
+    eplot,eax = plot.subplots()
+    eax.plot(t,eHist[0,:],color='orange',linewidth=2,linestyle='-')
+    eax.plot(t,eHist[1,:],color='blue',linewidth=2,linestyle='-')
+    eax.set_xlabel("$t$ $(sec)$")
+    eax.set_ylabel("$e_i$ $(rad)$")
+    eax.set_title("Error")
+    eax.legend(["$e_1$","$e_2$"],loc='upper right')
+    eax.grid()
+    eplot.savefig(path+"/error.pdf")
+
+    #plot the error norm
+    eNplot,eNax = plot.subplots()
+    eNax.plot(t,eNormHist,color='orange',linewidth=2,linestyle='-')
+    eNax.set_xlabel("$t$ $(sec)$")
+    eNax.set_ylabel("$\Vert e \Vert$ $(rad)$")
+    eNax.set_title("Error Norm")
+    eNax.grid()
+    eNplot.savefig(path+"/errorNorm.pdf")
+
+    #plot the anglular velocity
+    phiDplot,phiDax = plot.subplots()
+    phiDax.plot(t,phiDdHist[0,:],color='orange',linewidth=2,linestyle='--')
+    phiDax.plot(t,phiDHist[0,:],color='orange',linewidth=2,linestyle='-')
+    phiDax.plot(t,phiDdHist[1,:],color='blue',linewidth=2,linestyle='--')
+    phiDax.plot(t,phiDHist[1,:],color='blue',linewidth=2,linestyle='-')
+    phiDax.set_xlabel("$t$ $(sec)$")
+    phiDax.set_ylabel("$anglular velocity$ $(rad/sec)$")
+    phiDax.set_title("Anglular Velocity")
+    phiDax.legend(["$\dot{\phi}_{1d}$","$\dot{\phi}_1$","$\dot{\phi}_{2d}$","$\dot{\phi}_2$"],loc='upper right')
+    phiDax.grid()
+    phiDplot.savefig(path+"/anglularVelocity.pdf")
+
+    #plot the filtered error
+    rplot,rax = plot.subplots()
+    rax.plot(t,rHist[0,:],color='orange',linewidth=2,linestyle='-')
+    rax.plot(t,rHist[1,:],color='blue',linewidth=2,linestyle='-')
+    rax.set_xlabel("$t$ $(sec)$")
+    rax.set_ylabel("$r_i$ $(rad/sec)$")
+    rax.set_title("Filtered Error")
+    rax.legend(["$r_1$","$r_2$"],loc='upper right')
+    rax.grid()
+    rplot.savefig(path+"/filteredError.pdf")
+
+    #plot the filtered error norm
+    rNplot,rNax = plot.subplots()
+    rNax.plot(t,rNormHist,color='orange',linewidth=2,linestyle='-')
+    rNax.set_xlabel("$t$ $(sec)$")
+    rNax.set_ylabel("$\Vert r \Vert$ $(rad)$")
+    rNax.set_title("Filtered Error Norm")
+    rNax.grid()
+    rNplot.savefig(path+"/filteredErrorNorm.pdf")
+
+    #plot the anglular acceleration
+    phiDDplot,phiDDax = plot.subplots()
+    phiDDax.plot(t,phiDDdHist[0,:],color='orange',linewidth=2,linestyle='--')
+    phiDDax.plot(t,phiDDHist[0,:],color='orange',linewidth=2,linestyle='-')
+    phiDDax.plot(t,phiDDdHist[1,:],color='blue',linewidth=2,linestyle='--')
+    phiDDax.plot(t,phiDDHist[1,:],color='blue',linewidth=2,linestyle='-')
+    phiDDax.set_xlabel("$t$ $(sec)$")
+    phiDDax.set_ylabel("$anglular acceleration$ $(rad/sec^2)$")
+    phiDDax.set_title("Anglular Acceleration")
+    phiDDax.legend(["$\ddot{\phi}_{1d}$","$\ddot{\phi}_1$","$\ddot{\phi}_{2d}$","$\ddot{\phi}_2$"],loc='upper right')
+    phiDDax.grid()
+    phiDDplot.savefig(path+"/anglularAcceleration.pdf")
+
+    #plot the inputs
+    tauplot,tauax = plot.subplots()
+    tauax.plot(t,tauHist[0,:],color='orange',linewidth=2,linestyle='-')
+    tauax.plot(t,tauHist[1,:],color='blue',linewidth=2,linestyle='-')
+    tauax.plot(t,tauffHist[0,:],color='orange',linewidth=2,linestyle='--')
+    tauax.plot(t,tauffHist[1,:],color='blue',linewidth=2,linestyle='--')
+    tauax.plot(t,taufbHist[0,:],color='orange',linewidth=2,linestyle='-.')
+    tauax.plot(t,taufbHist[1,:],color='blue',linewidth=2,linestyle='-.')
+    tauax.set_xlabel("$t$ $(sec)$")
+    tauax.set_ylabel("$input$ $(Nm)$")
+    tauax.set_title("Control Input")
+    tauax.legend(['$\\tau_1$',"$\\tau_2$","$\\tau_{ff1}$","$\\tau_{ff2}$","$\\tau_{fb1}$","$\\tau_{fb2}$"],loc='upper right')
+    tauax.grid()
+    tauplot.savefig(path+"/input.pdf")
+
+    #plot the parameter estiamtes
+    thetaplot,thetaax = plot.subplots()
+    thetaax.plot(t,thetaTildaHist[0,:],color='red',linewidth=2,linestyle='-')
+    thetaax.plot(t,thetaTildaHist[1,:],color='green',linewidth=2,linestyle='-')
+    thetaax.plot(t,thetaTildaHist[2,:],color='blue',linewidth=2,linestyle='-')
+    thetaax.plot(t,thetaTildaHist[3,:],color='orange',linewidth=2,linestyle='-')
+    thetaax.plot(t,thetaTildaHist[4,:],color='magenta',linewidth=2,linestyle='-')
+    thetaax.set_xlabel("$t$ $(sec)$")
+    thetaax.set_ylabel("$\\tilde{\\theta}_i$")
+    thetaax.set_title("Parameter Error")
+    thetaax.legend(["$\\tilde{\\theta}_1$","$\\tilde{\\theta}_2$","$\\tilde{\\theta}_3$","$\\tilde{\\theta}_4$","$\\tilde{\\theta}_5$"],loc='upper right')
+    thetaax.grid()
+    thetaplot.savefig(path+"/thetaTilde.pdf")
+
+    #plot the parameter estiamtes norm
+    thetaNplot,thetaNax = plot.subplots()
+    thetaNax.plot(t,thetaNormHist,color='orange',linewidth=2,linestyle='-')
+    thetaNax.set_xlabel("$t$ $(sec)$")
+    thetaNax.set_ylabel("$\Vert \\tilde{\\theta} \Vert$")
+    thetaNax.set_title("Parameter Error Norm")
+    thetaNax.grid()
+    thetaNplot.savefig(path+"/thetaTildeNorm.pdf")
+
+
+
+    
